1b8ec730a22cc4e44ae966e1814330d33e3f3891
zhangyu <zhangyu@zhangyudeMBP.lan>
2016-01-19 00:22:00 +0800

    update

--- react/script/webpack.config.js
+++ react/script/webpack.config.js
@@ -5,13 +5,15 @@ var urlProject = fs.realpathSync('./');
 var node_modules = path.resolve(urlProject, 'node_modules');
 //console.log(111,__dirname);
 //console.log(222,urlProject);
+//path.resolve(urlProject, 'src/Main')
 config = {
     entry: [
         'webpack/hot/dev-server',
-        path.resolve(urlProject, 'src/Main')
+        path.resolve(urlProject, 'src/main2.js')
     ],
     resolve: {
-        extensions: ['', '.js', '.ts', '.tsx']
+        modulesDirectories: ['node_modules'],
+        extensions: ['', 'js', 'jsx', '.ts', '.tsx']
     },
     devtool: 'source-map',
     output: {
@@ -20,6 +22,16 @@ config = {
     },
     module: {
         loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader?presets[]=es2015"
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader?presets[]=es2015"
+            },
             {
                 test: /\.tsx?$/,
                 loader: 'ts-loader'
